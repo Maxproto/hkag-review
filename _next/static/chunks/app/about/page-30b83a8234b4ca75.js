05cbfd064474bd6a3afc3bfde804b45c3e3ec336
Max LAN <maxlan@Maxs-Laptop.local>
2026-07-30 21:35:38 -0700

    publish Hong Kong Art Groupe website

---
+++ _next/static/chunks/app/about/page-30b83a8234b4ca75.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[137,199,212,220,295,337,496,515,806,970,974,982],{744:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8500,23))}},e=>{e.O(0,[500,441,794,358],()=>e(e.s=744)),_N_E=e.O()}]);
